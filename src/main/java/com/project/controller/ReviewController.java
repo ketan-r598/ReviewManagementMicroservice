89e7408a54c80ef665fc422eeae4be6ca02c2b57
rkumar891 <rkumar891@US-124K9S3>
2023-06-20 12:08:25 +0530

    added @RequestMapping

--- src/main/java/com/project/controller/ReviewController.java
+++ src/main/java/com/project/controller/ReviewController.java
@@ -6,29 +6,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.model.Review;
 import com.project.service.ReviewService;
 
 @RestController
+@RequestMapping("/users/reviews/")
 public class ReviewController {
 
 	@Autowired
 	private ReviewService service;
 	
-	@PostMapping("/users/reviews/comment")
+	@PostMapping("comment")
 	public Review addReview(@RequestBody Review review) {
 		return service.addReview(review);
 	}
 	
-	@PutMapping("/users/reviews/editRatings/{reviewId}")
+	@PutMapping("editRatings/{reviewId}")
 	public Review editReview(@PathVariable("reviewId") int reviewId, @RequestBody Review review) {
 		return service.editReview(reviewId, review);
 		
 	}
 	
-	@DeleteMapping("/users/reviews/remove/{reviewId}")
+	@DeleteMapping("remove/{reviewId}")
 	public void deleteReview(@PathVariable int reviewId) {
 		service.deleteReview(reviewId);
 	}
