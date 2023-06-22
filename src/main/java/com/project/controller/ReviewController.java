97d363c5521c880a8a99b46fc8ffdcc20d8458b0
Ketan R <ketan_r598@outlook.com>
2023-06-22 16:13:44 +0530

    updated request mapping

--- src/main/java/com/project/controller/ReviewController.java
+++ src/main/java/com/project/controller/ReviewController.java
@@ -1,7 +1,10 @@
 package com.project.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,25 +16,30 @@ import com.project.model.Review;
 import com.project.service.ReviewService;
 
 @RestController
-@RequestMapping("/reviews/")
+@RequestMapping("/reviews/customer/")
 public class ReviewController {
 
 	@Autowired
 	private ReviewService service;
 	
-	@PostMapping("customer/comment")
+	@PostMapping("comment")
 	public Review addReview(@RequestBody Review review) {
 		return service.addReview(review);
 	}
 	
-	@PutMapping("customer/editRatings/{reviewId}")
+	@PutMapping("editRatings/{reviewId}")
 	public Review editReview(@PathVariable("reviewId") int reviewId, @RequestBody Review review) {
 		return service.editReview(reviewId, review);
 		
 	}
 	
-	@DeleteMapping("customer/remove/{reviewId}")
+	@DeleteMapping("remove/{reviewId}")
 	public void deleteReview(@PathVariable int reviewId) {
 		service.deleteReview(reviewId);
+	}
+	
+	@GetMapping("getAll")
+	public List<Review> getAllReview() {
+		return service.getAllReviews();
 	}
 }
